dac0458c198481a835a89b4c0646c4708ac10382
Lider2099 <meo20992089@gmail.com>
2022-02-02 17:48:41 +0100

    add hightlight

---
+++ pages/blog/maths/2F22_LesFonctionsComplexe.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.content
+    h1 Les fonctions ?
+    p On va reprendres les bases, je ne vais pas m'y atterder. Mais une fonction, en mathématique, c'est quoi ?
+    p Normalement, tout le monde à à l'idée f(x)=x
+        sup 2
+        | ou encore g(x)= 4x+2... Par exemple. Donc, en quelque sorte, une fonction c'est "on prends une valeur x" qui 
+        br 
+        | "se transforme en une autre valeur f(x) (ou g(x))"
+
+    p Bon, on sait, plus au moins, ce qu'est une fonction... Alors testons ! Je vais prendre la valeur où x=2 :
+    p Pour f(x), on a 4 car f(2) = 2
+        sup 2 
+        | = 4
+
+    p Mais pour g(x), on obtient 10 ! Car g(2) = 4×2+2 = 10
+
+    p Bon, je suis aller assez vite, car ce n'est pas ce qui m'intèresse. Moi, je parle de fonctions complexes. Mais nous ne savons pas ce qu'est un "nombre complexe"
+
+    h1 KESCé un nombre complexe ?
+
+    p En quelque sorte : 
+        strong i
+        sup 2 
+        | = 
+        strong -1
+
+    p Si vous ne voyez pas où est le problème, c'est ou que vous connaissez déjà, ou que vous ne connaissez qu'un nombre réel (donc 1,-1,5, racine de 2, ect..) NE FERA JAMAIS un negatif. 
+        | C'est assez facile, un nombre multiplier par lui même ne pourra jamais être negatif. 
+    p Donc c'est ça le "i" qui va se glisser partout. Mais on pourra aussi parler de "j" qui est dans le principe le même.
+    p Les nombres complexes se situ non plus sur une droite, mais sur un plan. Avec pour absicisse les réels, et ordonnée les imaginaires. On notera donc que le point z
+        sub 1
+        | = 1+3j à pour absicisse 1 et ordonnée 3. (et non "3j" car on prends juste le nombre avant le "j"... sans le "j").
+
+    p Alors, avec ça, on peut faire plein de chose ! Comme déjà, aimer les maths ! Mais aussi, prendre un dolipran. Parce que là, on va passer au dur, un mélange des 2 !
+
+    h1 les fonctions complexes
+    p Bon, je vais vous avouez que je ne connais pas non plus le terme exacte. 
+    p Pour ce qui va suivre, on va utiliser python, je ne vais expliquer ce que c'est... Internet est là, mais en quelque sorte, c'est un langage de programation assez simple.
+    p Nous allons, enfin, débuter. 
+
+    p pour débuter, on va devoir mettre ce petit bout de code, dès le debut ! 
+    pre 
+        code(lang="python")
+            
+</template>
--- store/index.js
+++ store/index.js
@@ -2,11 +2,11 @@ export const state = () => ({
     categories: ["math", "web", "python", "programation", "livre", "design", "modelisation"],
     UpCat: ["math", "web", "livre", "design"], //Max 4 ! Sinon, c'est over
     blogs: ([{
-        name: "coucou",
+        name: "Fonctions complexes",
         tag: ["math"],
-        description: "j'aime les maths !",
-        url: "/maths/coucou",
-        date: "2022-01-29"
+        description: "Dessiner des fonctions complexes sur python !",
+        url: "/maths/2F22_LesFonctionsComplexe",
+        date: "2022-02-02"
     },{
         name: "Les maths, c'est magiques !",
         tag: ["math", "web"],
