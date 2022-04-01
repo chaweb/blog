3c3254abb8bd5ae03fa396cd0043c9b094f4efae
chaweb <46398015+chaweb@users.noreply.github.com>
2022-04-01 20:18:59 +0200

    maj

--- layouts/default.vue
+++ layouts/default.vue
@@ -41,8 +41,7 @@ div
                 br
                 |This work is licensed under.
 
-            p
-                a(rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/") Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+            p Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 
 </template>
 
