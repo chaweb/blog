ce2034eeac500d2ea81274bf4691f15f3abf4b21
chaweb <46398015+chaweb@users.noreply.github.com>
2022-04-11 10:51:29 +0200

    corrections des fautes

--- pages/bio.vue
+++ pages/bio.vue
@@ -4,31 +4,33 @@
   h2 Qui suis-je ?
   p Je ne sais pas trop comment je devrais me présenter, mais je vais essayer 
     | d'être simple. Je m'appelle Charlie, mais mon pseudo est Chaweb. Je suis étudiante, encore. 
-    | Je suis arrivée dans le web en commencant un peu bêtement, avec scratch - le compte encore ouvert 
+    | Je suis arrivée dans le web en commençant un peu bêtement, avec scratch - le compte est encore ouvert 
     | (sur un autre pseudo). 
   p J'avais soif de savoir, de découvrir ce monde, sur le quel je n'étais qu'utilisatrice. 
     | Ce monde de création, d'art via des simples lignes de code. 
-    | Donc, je me suis formée au python pendant les grandes vacances d'entre 5-4ème (france)... 
+    | Donc, je me suis auto-formée au python pendant les grandes vacances d'entre 5-4ème (france)... 
     | Ce qui n'a rien à voir avec le web. Puis, le livre ! Le livre qui a changer ma perception... Bon, j'exagère.
-    | Ce livre ne parlais qu'HTML et Css. Pendant quelques années, je ne fessais que ça. 
-  p Puis, il est arrivé le confinement durant la fin de 2nd en 2020, à cause d'une pandemie mondiale. 
-    | Bien que beaucoup de négatif soit ressortit de cette crise, cela m’a permis d’apprendre encore plus. Sur le javascript (JS) principalement.
-    | D'abord des petits bots discord avec nodejs, puis du js pur et simple avec HTML et Css. 
+    | Ce livre ne parlait qu'HTML et Css. Pendant quelques années, je ne fessais que ça. 
+  p Puis, il est arrivé le confinement durant la fin ma 2nd en 2020, à cause d'une pandemie mondiale. 
+    | Bien que beaucoup de négatif soit ressortit de cette crise, cela m’a permise d’apprendre encore plus. Sur le javascript (JS) principalement.
+    | D'abord des petits bots Discord avec nodejs, puis du js pur et simple avec HTML et Css. 
     | Pour finir sur du VueJs l'année d'après. Et apprendre le monde de Nuxtjs ! 
 
-  p Aujourd'hui, et ce site en fait, j'utilise Nuxt.js avec Pug et Sass (un peu de terme technique si vous ne connaissez pas bien le millieu, sowwy). 
-    | Et donc, c'est aussi pour m'entrainer, m'excercer et taffer sur un site.     
+  p Aujourd'hui, et ce site est fait avec, j'utilise Nuxtjs avec Pug et Sass (un peu de terme technique si vous ne connaissez pas bien le millieu, 
+    i sowwy
+    |). 
+    | Et donc, c'est aussi pour m'entrainer, m'excercer et travailler sur un site.     
 
   //- sujet du site
   h2 Un blog
 
   p Ce blog contiendra un peu de tout. Allant du tuto "comment faire tel chose" à, si je me sens assez à l'aise, juste des critique  
-    | contre la société ou des personnes polytiques, ou même des histoires oneshot relier à mon univer original.
+    | contre la société, des ressenties que j'ai avec le monde exterieur, ou même des histoires oneshot relier à mon univer original.
 
   p Les parties seront séparées en catégorie, pour que vous puissiez lire ce que vous voulez. 
 
   p Je tiens à préciser que même si j'aime ce que j'écris, certaine chose peut être fausse... Donc, dans ce que je dis 
-    | -Et j'espère que vous le faites déjà- ne prenez au pied de la lettre. Soyez critique. 
+    | -Et j'espère que vous le faites déjà- ne prenez pas au pied de la lettre. Soyez critique. 
 
   //-outro
   h2 bonne lecture
--- pages/blog/maths/2F22_LesFonctionsComplexe.vue
+++ pages/blog/maths/2F22_LesFonctionsComplexe.vue
@@ -2,11 +2,11 @@
 .content
     h1 Les fonctions ?
     p On va reprendre les bases, je ne vais pas m'y atterder. Mais une fonction, en mathématique, c'est quoi ?
-    p Normalement, tout le monde à à l'idée f(x)=x
+    p Normalement, tout le monde a à l'idée f(x)=x
         sup 2
-        | ou encore g(x)= 4x+2... Par exemple. Donc, en quelque sorte, une fonction c'est "on prends une valeur x" qui 
+        | ou encore g(x)= 4x+2, par exemple. Donc, en quelque sorte, une fonction c'est "on prends une valeur x" qui 
         br 
-        | "se transforme en une autre Variable f(x) (ou g(x))"
+        | "se transforme en une valeur : f(x) (ou g(x))"
 
     p Bon, on sait, plus au moins, ce qu'est une fonction... Alors testons ! Je vais prendre la valeur où x=2 :
     p Pour f(x), on a 4 car f(2) = 2
@@ -15,7 +15,7 @@
 
     p Mais pour g(x), on obtient 10 ! Car g(2) = 4×2+2 = 10
 
-    p Bon, je suis aller assez vite, car ce n'est pas ce qui m'intèresse. Moi, je parle de fonctions complexes. Mais nous ne savons pas ce qu'est un "nombre complexe"
+    p Bon, je suis aller assez vite, car ce n'est pas ce qui m'intèresse. Je parle de fonctions complexes dans mon blog. Mais nous ne savons pas ce qu'est un "nombre complexe"
 
     h1 KESCé un nombre complexe ?
 
@@ -25,14 +25,16 @@
         | = 
         strong -1
 
-    p Si vous ne voyez pas où est le problème, c'est ou que vous connaissez déjà, ou que vous ne connaissez qu'un nombre réel (donc 1,-1,5, racine de 2, ect..) NE FERA JAMAIS un negatif. 
-        | C'est assez facile, un nombre multiplier par lui même ne pourra jamais être negatif. Car un nombre négatif multiplié par un autre nombre négatif nous donneras un nombre positif suivant la loi des signes
-    p Donc c'est ça le "i" qui va se glisser partout. Mais on pourra aussi parler de "j" qui est dans le principe le même.
-    p Les nombres complexes se situ non plus sur une droite, mais sur un plan. Avec pour absicisse les réels, et ordonnée les imaginaires. On notera donc que le point z
+    p Si vous ne voyez pas où est le problème, c'est ou que vous connaissez déjà, ou que vous ne savez pas qu'un nombre réel (donc 1,-1,5, racine de 2, ect..) NE FERA JAMAIS un negatif. 
+        | C'est assez facile, un nombre multiplier par lui même ne pourra jamais être negatif. Car un nombre négatif multiplié par un autre nombre négatif nous donneras un nombre positif suivant la loi des signes.
+    p Donc c'est ça le "i" qui va se glisser partout. Mais on pourra aussi parler de "j" qui est dans le principe le même mais en physique.
+    p Les nombres complexes se situe non plus sur une droite, mais sur un plan. Avec pour absicisse les réels, et ordonnée les imaginaires. On notera donc que l'affix du point z
         sub 1
-        | = 1+3j à pour absicisse 1 et ordonnée 3. (et non "3j" car on prends juste le nombre avant le "j"... sans le "j").
+        | = 1+3j à pour absicisse 1 et ordonnée 3 z
+        sub 1
+        |(1;3). (et non "3j" car on prends juste le nombre avant le "j"... sans le "j").
 
-    p On parlera aussi de "parti imaginaire" qui justement la valeur de l'ordonnée et de "parti réel" qui est la valeur réelle
+    p On parlera aussi de "parti imaginaire" qui justement est la valeur de l'ordonnée et de "parti réelle" qui est la valeur des absicisses.
 
     p Alors, avec ça, on peut faire plein de chose ! Comme déjà, aimer les maths ! Mais aussi, prendre un dolipran. Parce que là, on va passer au dur, un mélange des 2 !
 
@@ -51,8 +53,10 @@
             |import plotly.graph_objects as go  #pour dessiner de jolie dessin (alias, les graphs)
             |import numpy as np                 #pour les calculs un peu compliqué
             |import math                        #pareille parce que c'est bien ! 
-    p Comme ça, j'ai envie de dire : c'est fait ! Si vous n'avez pas le module directement d'installer -je sais que sur google colab c'est déjà installer- je vous conseille de tout installer, en recherchant sur internet les modules.
-    p On va commencer un peu un coder, enfin. A initier notre fonction !
+    p Comme ça, j'ai envie de dire : c'est fait ! Si vous n'avez pas le module directement d'installer -je sais que sur 
+    a(href="https://colab.research.google.com") google colab
+    |  c'est déjà installer- je vous conseille de tout installer, en recherchant sur internet les modules.
+    p On va commencer un peu à coder, enfin... A initier notre fonction !
     pre
         code(v-highlight).py
             |def function(x): #definir la fonction math
@@ -65,17 +69,17 @@
     br
     p Donc, d'abord, on initialise un tableau "x" pour qu'elle prenne pour entre 20 et -20 : 
     pre
-        code(v-highlight).py x = [y/5 for y in range(-100,101)]
-    p avec un pas de 1/5.
-    p Puis, dans un tableau "x_imaginaire" on y rajoute A fois la longueur de "x". Pour enfin, nous rappatre sur un plan de [-20;20] pour x et de [-20;20] pour y et de pas de 0.2.
+        code(v-highlight).py x = [y/5 for y in range(-100,101)] #creation d'une liste prenant entre -100/5 = -20 inclu et 101/5 = 20,2 exclu cela prend jusqu'à la valeur 100/5 = 20.
+    p avec un pas de 1/5 (0,2).
+    p Puis, dans un tableau "x_imaginaire" on y rajoute à fois la longueur de "x". Pour enfin, nous mettre sur un plan de [-20;20] pour x et de [-20;20] pour y et de pas de 0.2.
     pre
         code(v-highlight).py x_imaginaire = x*len(x)
-    p Dans -encore- un autre tableau, on y met les "x_imaginaire" mais cette fois trier. (cela va être sans doute plus logique à la fin...)
+    p Dans -encore- un autre tableau, on y met les "x_imaginaire" mais cette fois trier. Permettant d'avoir des valeurs en boucle sur Y et des valeurs trier sur X (cela va être sans doute plus logique à la fin...)
     pre
         code(v-highlight).py x_real = sorted(x_imaginaire)
 
     p ENFIN ! On a notre plan, sur le quel on aura toutes nos valeurs. Et logiquiment, ensuite, nos resultats.
-    p Mais ce n'est pas fini, il reste le tableau "x" qu'il faut qu'on change. Pour cela, l'initialise pour être à une valeur (comme 0) pour pouvoir le re-changer et mettre le nombre entièrement avec sa partie réelle et sa partie imaginaire.
+    p Mais ce n'est pas fini, il reste le tableau "x" qu'il faut qu'on change. Pour cela, On initialise à une valeur (comme 0) pour pouvoir le re-changer et mettre le nombre entièrement avec sa partie réelle et sa partie imaginaire.
     pre
         code(v-highlight).py
             |x=[0 for x in x_real]
@@ -87,11 +91,11 @@
     
     h3 LE TABLEAU !
 
-    p Avant, cela, regardant la longueur de "x" pour voir : 
+    p Avant, cela, regard-on la longueur de "x" pour voir : 
     pre
         code(v-highlight).py print(len(x))
-    p ça fait beaucoup ? Bon, en vrai, faut avoir de la ram : c'est sur. Mais ça peut aller.
-    p Mainenant, on va mettre les resultat dans 2 tableaux distincts : 
+    p ça fait beaucoup ? Bon, en vrai, faut avoir de la ram : c'est sur. Mais ça peut aller si vous êtes avec Gcolab.
+    p Mainenant, on va mettre les resultats dans 2 tableaux distincts : 
         
         li  une pour la valeur réelle
         
@@ -102,7 +106,7 @@
             |y_real = [(function(x)).real for x in x] #parti réelle des resultats
             |y_imaginaire= [(function(x)).imag for x in x] #parti imaginaire des resultats
 
-    p Enfin, on met tout ça dans un tableau "panda" ou comme on l'a raccourcis "pd"
+    p Enfin, on met tout ça dans un tableau "panda" ou comme on l'a mis en raccourcis : "pd"
     pre
         code(v-highlight).py
             |df = pd.DataFrame({'x real':x_real,
@@ -110,10 +114,11 @@
             |       'y real':y_real,
             |       'y imaginaire':y_imaginaire})
 
-    p En faisant 
+    p En faisant
     pre
         code(v-highlight).py df #ou print(df)
-    p Maintenant, nous allons tracer ! On a notre tableau, nos resultat... Donc, c'est bon ! On peut tracer.
+    p peut voir toutes les valeurs brutes. Nous permet de voir si notre tableau n'a pas d'erreur.  
+    p Maintenant, nous allons tracer ! On a notre tableau, nos resultats... Donc, c'est bon ! On peut tracer.
 
     pre
         code(v-highlight).py
@@ -121,8 +126,9 @@
             |                      go.Scatter3d(x=df['x imaginaire'], y=df['x real'], z=df['y imaginaire'],mode='markers', name="imaginaire")])
             |fig.show() #tracer le graph
     p Voilà, je vous laisse admirer votre travail... Ou le debug, si cela ne fonctionne pas.
+    p Si vous voulez changer de fonctions, pour en admirer d'autres, il faut juste changer la fonction du début qu'on avait employer.
     
-    p Sinon, je peux vous laisser avec quelque fonction choisis que j'ai trouver assez stylé :
+    p Sinon, je peux vous laisser avec quelques fonctions choisis que j'ai trouver assez stylé (attention, cette fin ne pourra pas être vue par tout le monde. Car ce n'est pas très optimiser... J'arrive avec une mis à jour pour réglé se problème):
     .card.block
         header.card-header 
             p.card-header-title f(x)=x
